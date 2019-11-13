ee74616ad9ab1ad4d7eff0e8913a6253661b361e
Enrick De Munter <enrick.demunter@student.romerocollege.be>
2019-11-13 09:16:43 +0100

    2de commit

--- 17delen/Program.cs
+++ 17delen/Program.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿// Enrick De Munter 5ITN nr4
+
+/*we willen dat alle getallen deelbaar zijn
+ * 
+ * input
+ * Vraag het deeltal aan de gebruiker
+ * Vraag het getal dat je wil laten delen door het deeltal
+ * 
+ * processing
+ * Doe de deling
+*/
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +24,7 @@ namespace _17delen
         static void Main(string[] args)
         {
             //Declaraties
-            byte deeltal, getal;
+            byte deeltal, deler;
             string antwoord;
             bool vraagdeeltal, vraaggetal;
 
@@ -21,11 +34,11 @@ namespace _17delen
             vraagdeeltal = byte.TryParse(Console.ReadLine(), out deeltal);
             // Vraag het getal dat je wil laten delen door het deeltal
             Console.WriteLine("wat is uw getal?: ");
-            vraaggetal = byte.TryParse(Console.ReadLine(), out getal);
+            vraaggetal = byte.TryParse(Console.ReadLine(), out deler);
 
             //processing
             // Doe de deling tussen de 2 getallen
-            antwoord = vraagdeeltal / vraaggetal;
+            antwoord = 
 
             // output
             //Toon de het eindegetal na de deling
